e63ef9e78cab7930972bd6327c04b80db50687f4
Pietro Rhyan <pietroqjg@gmail.com>
2022-11-08 23:25:32 -0300

    Adding User account, refactorin with tsyringe and adding token jwt

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
+import { authenticateRoutes } from './Authenticate.routes';
 import { categoriesRoutes } from './Categories.routes';
 import { specificationRoutes } from './Specifications.routes';
+import { usersRoutes } from './Users.routes';
 
 const router = Router();
 
 router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationRoutes);
+router.use('/users', usersRoutes);
+router.use(authenticateRoutes);
 
 export { router };
