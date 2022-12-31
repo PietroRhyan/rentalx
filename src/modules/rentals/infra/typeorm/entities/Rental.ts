2bf844e218de2f11ddc120e8f71213c2de77ffd1
Pietro Rhyan <pietroqjg@gmail.com>
2022-12-30 22:22:02 -0300

    Creating documentation to users authentications

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,7 +1,9 @@
 import {
-  Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn,
+  Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn, UpdateDateColumn,
 } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
+
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
 
 @Entity('rentals')
 class Rental {
@@ -10,6 +12,10 @@ class Rental {
 
   @Column()
     car_id: string;
+
+  @ManyToOne(() => Car)
+  @JoinColumn({ name: 'car_id' })
+    car: Car;
 
   @Column()
     user_id: string;
